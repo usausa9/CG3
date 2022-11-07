a4e3340b52b2f393093d87c64ee30e709f324e24
Wsd kazu <grazie.taiko@gmail.com>
2022-11-07 18:31:21 +0900

    確認課題OK

--- Object3d.cpp
+++ Object3d.cpp
@@ -25,12 +25,13 @@ CD3DX12_CPU_DESCRIPTOR_HANDLE Object3d::cpuDescHandleSRV;
 CD3DX12_GPU_DESCRIPTOR_HANDLE Object3d::gpuDescHandleSRV;
 XMMATRIX Object3d::matView{};
 XMMATRIX Object3d::matProjection{};
-XMFLOAT3 Object3d::eye = { 0, 0, -50.0f };
+XMFLOAT3 Object3d::eye = { 0, 0, -5.0f };
 XMFLOAT3 Object3d::target = { 0, 0, 0 };
 XMFLOAT3 Object3d::up = { 0, 1, 0 };
 D3D12_VERTEX_BUFFER_VIEW Object3d::vbView{};
 //D3D12_INDEX_BUFFER_VIEW Object3d::ibView{};
-Object3d::VertexPosNormalUv Object3d::vertices[vertexCount];
+//Object3d::VertexPosNormalUv Object3d::vertices[vertexCount];
+Object3d::VertexPos Object3d::vertices[vertexCount];
 //unsigned short Object3d::indices[planeCount * 3];
 //unsigned short Object3d::indices[indexCount];
 
@@ -272,16 +273,16 @@ void Object3d::InitializeGraphicsPipeline()
 			D3D12_APPEND_ALIGNED_ELEMENT,
 			D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0
 		},
-		{ // 法線ベクトル(1行で書いたほうが見やすい)
-			"NORMAL", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0,
-			D3D12_APPEND_ALIGNED_ELEMENT,
-			D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0
-		},
-		{ // uv座標(1行で書いたほうが見やすい)
-			"TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0,
-			D3D12_APPEND_ALIGNED_ELEMENT,
-			D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0
-		},
+		//{ // 法線ベクトル(1行で書いたほうが見やすい)
+		//	"NORMAL", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0,
+		//	D3D12_APPEND_ALIGNED_ELEMENT,
+		//	D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0
+		//},
+		//{ // uv座標(1行で書いたほうが見やすい)
+		//	"TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0,
+		//	D3D12_APPEND_ALIGNED_ELEMENT,
+		//	D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0
+		//},
 	};
 
 	// グラフィックスパイプラインの流れを設定
@@ -438,7 +439,7 @@ void Object3d::CreateModel()
 {
 	HRESULT result = S_FALSE;
 
-	std::vector<VertexPosNormalUv> realVertices;
+	std::vector<VertexPos> realVertices;
 	//// 頂点座標の計算（重複あり）
 	//{
 	//	realVertices.resize((division + 1) * 2);
@@ -567,8 +568,8 @@ void Object3d::CreateModel()
 
 	//std::copy(std::begin(verticesSquare), std::end(verticesSquare), vertices);
 
-	VertexPosNormalUv verticesPoint[] = {
-		{{0.0f,0.0f,0.0f},{0,0,1},{0,1}},
+	VertexPos verticesPoint[] = {
+		{{0.0f,0.0f,0.0f}}
 	};
 
 	std::copy(std::begin(verticesPoint), std::end(verticesPoint), vertices);
@@ -595,7 +596,7 @@ void Object3d::CreateModel()
 	assert(SUCCEEDED(result));
 
 	// 頂点バッファへのデータ転送
-	VertexPosNormalUv* vertMap = nullptr;
+	VertexPos* vertMap = nullptr;
 	result = vertBuff->Map(0, nullptr, (void**)&vertMap);
 	if (SUCCEEDED(result)) {
 		memcpy(vertMap, vertices, sizeof(vertices));
@@ -789,8 +790,9 @@ void Object3d::Update()
 	// 定数バッファへデータ転送
 	ConstBufferData* constMap = nullptr;
 	result = constBuff->Map(0, nullptr, (void**)&constMap);
-	constMap->color = color;
-	constMap->mat = matWorld * matView * matProjection;	// 行列の合成
+	//constMap->color = color;
+	//constMap->mat = matWorld * matView * matProjection;	// 行列の合成
+	constMap->mat = matView * matProjection;
 	constBuff->Unmap(0, nullptr);
 }
 
--- Resources/shaders/BasicGeometryShader.hlsl
+++ Resources/shaders/BasicGeometryShader.hlsl
@@ -1,46 +1,34 @@
 #include "BasicShaderHeader.hlsli"
 
-//struct GSOutput
-//{
-//	float4 pos : SV_POSITION;
-//};
+static const uint vnum = 4;
 
-[maxvertexcount(3)]
+static const float4 offset_array[vnum] =
+{
+	float4(-0.5f,-0.5f,0,0),
+	float4(-0.5f,+0.5f,0,0),
+	float4(+0.5f,-0.5f,0,0),
+	float4(+0.5f,+0.5f,0,0)
+};
+
+static const float2 uv_array[vnum] =
+{
+	float2(0,1),
+	float2(0,0),
+	float2(1,1),
+	float2(1,0)
+};
+
+[maxvertexcount(vnum)]
 void main(
 	point VSOutput input[1] : SV_POSITION, 
 	inout TriangleStream< GSOutput > output
 )
 {
-
 	GSOutput element;
-	element.normal = input[0].normal;
-	element.uv = input[0].uv;
-
-	element.svpos = input[0].svpos;
-	output.Append(element);
-
-	element.svpos = input[0].svpos + float4(10.0f, 10.0f, 0.0f, 0.0f);
-	output.Append(element);
-
-	element.svpos = input[0].svpos + float4(10.0f, 0.0f, 0.0f, 0.0f);
-	output.Append(element);
-
-	//for (uint i = 0; i < 3; i++)
-	//{
-	//	GSOutput element;
-	//	element.svpos = input[i].svpos;
-	//	element.normal = input[i].normal;
-	//	element.uv = input[i].uv;
-	//	output.Append(element);
-	//}
-	//output.RestartStrip();
-	//for (uint i = 0; i < 3; i++)
-	//{
-	//	GSOutput element;
-	//	element.svpos = input[i].svpos + float4(20.0f, 0.0f, 0.0f, 0.0f);
-	//	element.normal = input[i].normal;
-	//	element.uv = input[i].uv * 5.0f;
-	//	output.Append(element);
-	//}
-
+	for (uint i = 0; i < vnum; i++) {
+		element.svpos = input[0].pos + offset_array[i];
+		element.svpos = mul(mat, element.svpos);
+		element.uv = uv_array[i];
+		output.Append(element);
+	}
 }
